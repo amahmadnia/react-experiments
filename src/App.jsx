680fb1775f2813ba93bd3aec4d8b34d40cafa345
Amirabas Ahmadnia <ahmadniaamirabas@gmail.com>
2024-07-09 19:51:35 +0330

    feat: testing formik validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,15 @@
-import usePersonStore from './store/usePersonStore';
-import useCounterStore from './store/useCounterStore';
+import SignupForm from './components/Formik1';
+import SignupForm2 from './components/Formik2';
 
 function App() {
-  // "select" the needed state and actions, in this case, the firstName value
-  // and the action updateFirstName
-  const firstName = usePersonStore((state) => state.firstName);
-  const updateFirstName = usePersonStore((state) => state.updateFirstName);
-
   return (
     <main>
-      <label>
-        First name
-        <input
-          // Update the "firstName" state
-          onChange={(e) => updateFirstName(e.currentTarget.value)}
-          value={firstName}
-        />
-      </label>
-
-      <p>
-        Hello, <strong>{firstName}!</strong>
-      </p>
+      <h2>Formik Test 1</h2>
+      <SignupForm />
+      <br />
+      <h2>Formik Test 2</h2>
+      <SignupForm2 />
+      <br />
     </main>
   );
 }
---
+++ src/components/ZustandTest.jsx
@@ -0,0 +1,29 @@
+import usePersonStore from './../store/usePersonStore';
+import useCounterStore from './../store/useCounterStore';
+
+function ZustandTest() {
+  // "select" the needed state and actions, in this case, the firstName value
+  // and the action updateFirstName
+  const firstName = usePersonStore((state) => state.firstName);
+  const updateFirstName = usePersonStore((state) => state.updateFirstName);
+
+  return (
+    <main>
+      <hr />
+      <label>
+        First name
+        <input
+          // Update the "firstName" state
+          onChange={(e) => updateFirstName(e.currentTarget.value)}
+          value={firstName}
+        />
+      </label>
+
+      <p>
+        Hello, <strong>{firstName}!</strong>
+      </p>
+    </main>
+  );
+}
+
+export default ZustandTest;
